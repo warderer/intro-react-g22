b4b0af3175bd60fd3bcf13b0a5564b6eefe99f4e
César Guerra <warderer@gmail.com>
2023-04-18 20:47:03 -0600

    ✨ Mi Primer Componente

---
+++ 01-vite-vanilla/src/components/HolaMundo.js
@@ -0,0 +1,5 @@
+const HolaMundo = () => `
+    <h1>Hola Dev.F Gen 22</h1>
+    <p>Contenido desde un Template String</p>
+`
+export default HolaMundo
--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,20 +1,8 @@
 import './styles/style.css'
-import javascriptLogo from './assets/javascript.svg'
+import HolaMundo from './components/HolaMundo.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hola Dev.F Gen 22</h1>
-    <p>Bienvenidos</p>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+  ${HolaMundo()}
 `
 
 
