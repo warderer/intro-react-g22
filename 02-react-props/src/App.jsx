c9cece5e82b6ef3e9b3d74d1854d6d57de6b3b7b
César Guerra <warderer@gmail.com>
2023-04-20 21:15:32 -0600

    ✨ Implementando Plantilla

--- 02-react-props/src/App.jsx
+++ 02-react-props/src/App.jsx
@@ -1,37 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../vite.svg'
 import './App.css'
 import HolaMundo from './components/HolaMundo'
+import ilustracion1 from './assets/img/ilustracion1.svg'
+import ilustracion from './assets/img/ilustracion.svg'
+import uno from './assets/img/uno.jpg'
+import dos from './assets/img/dos.jpg'
+import tres from './assets/img/tres.jpg'
+import cuatro from './assets/img/cuatro.jpg'
+import cinco from './assets/img/cinco.jpg'
+import seis from './assets/img/seis.jpg'
 
 function App () {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>DevF + G22</h1>
+      <header className='hero'>
+        <div className='textos-hero'>
+          <h1>Bienvenido a Website</h1>
+          <p>Creamos el mejor sitio web para ti</p>
+          <a href='#contacto'>Contactame</a>
+        </div>
+        <div className='svg-hero' style={{ height: '150px', overflow: 'hidden' }}>
+          <svg
+            viewBox='0 0 500 150' preserveAspectRatio='none'
+            style={{ height: '100%', width: '100%' }}
+          >
+            <path
+              d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
+              style={{ stroke: 'none', fill: '#fff' }}
+            />
+          </svg>
+        </div>
+      </header>
+
+      <section className='wave-contenedor website'>
+        <img src={ilustracion1} alt='' />
+        <div className='contenedor-textos-main'>
+          <h2 className='titulo left'>Title of section</h2>
+          <p className='parrafo'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt enim reiciendis
+            molestias nam tempore. Ullam hic accusantium eligendi ipsam corrupti!
+          </p>
+          <a href='' className='cta'>Learn more</a>
+        </div>
+      </section>
+
+      <section className='info'>
+        <div className='contenedor'>
+          <h2 className='titulo left'>Juntos podemos apoyar</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </section>
+
+      <section className='cards contenedor'>
+        <h2 className='titulo'>Our services</h2>
+        <div className='content-cards'>
+          <article className='card'>
+            <i className='far fa-clone' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>Learn more</a>
+          </article>
+          <article className='card'>
+            <i className='fas fa-database' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>Learn more</a>
+          </article>
+          <article className='card'>
+            <i className='far fa-object-group' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>Learn more</a>
+          </article>
+        </div>
+      </section>
+
+      <section className='galeria'>
+        <div className='contenedor'>
+          <h2 className='titulo'>Our work</h2>
+          <article className='galeria-cont'>
+            <img src={uno} alt='' />
+            <img src={dos} alt='' />
+            <img src={tres} alt='' />
+            <img src={cuatro} alt='' />
+            <img src={cinco} alt='' />
+            <img src={seis} alt='' />
+          </article>
+        </div>
+      </section>
+
+      <section className='info-last'>
+
+        <div className='contenedor last-section'>
+          <div className='contenedor-textos-main'>
+            <h2 className='titulo left'>Title of section</h2>
+            <p className='parrafo'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum reprehenderit nostrum expedita quasi odio architecto laudantium sunt nemo dicta atque?</p>
+            <a href='' className='cta'>Learn more</a>
+          </div>
+          <img src={ilustracion} alt='' />
+        </div>
+
+        <div className='svg-wave' style={{ height: '150px', overflow: 'hidden' }}>
+          <svg
+            viewBox='0 0 500 150' preserveAspectRatio='none'
+            style={{ height: '100%', width: '100%' }}
+          >
+            <path
+              d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
+              style={{ stroke: 'none', fill: '#f5576c' }}
+            />
+          </svg>
+        </div>
+      </section>
+
+      <footer id='contacto'>
+        <div className='contenedor'>
+          <h2 className='titulo'>Contact us</h2>
+          <form action='' className='form'>
+            <input className='input' type='text' name='' id='' placeholder='Nombre' />
+            <input className='input' type='email' name='' id='' placeholder='Email' />
+            <textarea className='input' name='' id='' cols='30' rows='10' placeholder='Mensaje' />
+            <input className='input' type='submit' value='Enviar' />
+          </form>
+        </div>
+      </footer>
       <HolaMundo saludo='Hola G22' nombre='Pedro' />
       <HolaMundo saludo='Holis' nombre='Enriqueta' />
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1>Hola</h1>
     </>
   )
 }
