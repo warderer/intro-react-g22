7182209e24e09ce7adb28f182ce1cbc9515a65ee
César Guerra <warderer@gmail.com>
2023-04-27 20:13:37 -0600

    ✨ useEffect Base

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App () {
+  const [gifs, setGifs] = useState([])
+  const APIKEY = import.meta.env.VITE_GIPHY_API_KEY
+  // Las llamadas a API ocurren normalmente en el useEffect.
+  // useEffect usa un arreglo de dependencias vacio para que solo se ejecute una vez.
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=30&rating=g`)
+      .then(res => res.json())
+      .then(results => {
+        const { data } = results
+        setGifs(data)
+      }).catch(err => console.log(err))
+  }, [])
+
   return (
     <>
       <div>
