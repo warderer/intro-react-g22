3c8a22b0d47584bd5abf7cf40a47de36b8f98ab0
César Guerra <warderer@gmail.com>
2023-05-04 19:38:17 -0600

    ✨ Listado de Pokemons

--- 05-poke-bootstrap/src/pages/Home.jsx
+++ 05-poke-bootstrap/src/pages/Home.jsx
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react'
+
 const Home = () => {
+  const [pokemons, setPokemons] = useState([]) // Lista de Pokémons
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then(data => setPokemons(data.results))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
-    <h1>Home</h1>
+    <div className='container'>
+      <h1>Home</h1>
+      <div className='row'>
+        {
+            pokemons.map(pokemon => (
+              <div className='col-sm-4 mb-4' key={pokemon.name}>
+                <div className='card'>
+                  <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} className='card-img-top' alt={pokemon.name} />
+                  <div className='card-body'>
+                    <h5 className='card-title'>{pokemon.name}</h5>
+                  </div>
+                </div>
+              </div>
+            ))
+        }
+      </div>
+    </div>
   )
 }
 export default Home
